f9c2c6ebe40e3887c7b68c7e9ad4711ca13a3e97
Gonzalo Montiel <gonzalo.montiel06@outlook.com>
2021-09-23 18:11:33 -0300

    creando primer componente

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Componente from './primerComponente';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <App />
+      <Componente />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/primerComponente.js
@@ -0,0 +1,8 @@
+function Componente(){
+    return(
+        <div>
+            <h1>Primer componente</h1>
+        </div>)
+}
+
+export default Componente;
