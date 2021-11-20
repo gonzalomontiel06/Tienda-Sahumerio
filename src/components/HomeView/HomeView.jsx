bca85307b497331164903ed69b9a8d4d109e7487
Gonzalo Montiel <gonzalo.montiel06@outlook.com>
2021-11-20 13:58:59 -0300

    Netlify

--- src/components/HomeView/HomeView.jsx
+++ src/components/HomeView/HomeView.jsx
@@ -9,7 +9,7 @@ export const HomeView = () => {
 
         // HOME VIEW 
 
-        <article className='container homeView'>
+        <article className='container mt-3 homeView'>
             <div className='homeView__box'>
                 <NavLink exact to='/productos'>
                     <button className='animate__animated animate__bounce animate__delay-1s animate__infinite homeView__box__button'>Shop now!</button>
