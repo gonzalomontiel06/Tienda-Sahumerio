f66b46db45ebd3c752407631fc5c40b68f575468
Gonzalo Montiel <gonzalo.montiel06@outlook.com>
2022-05-24 21:02:52 -0300

    priceFilter

--- src/components/PriceFilter/PriceFilter.jsx
+++ src/components/PriceFilter/PriceFilter.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
+import './priceFilter.scss'
 import { NavLink } from 'react-router-dom'
 
 export const PriceFilter = () => {
 
     return (
-        <>
-            {/* <div>
-                <NavLink exact to='/orden/menorPrecio'>
+        <div className='asd mt-4'>
+            <h4 className='subtitle'>ordenar por</h4>
+            <div className='price'>
+                <NavLink activeClassName={'categorieSelected'} exact to='/productos'>
+                    Mas relevante
+                </NavLink>
+                <NavLink activeClassName={'categorieSelected'} exact to='/orden/menorPrecio'>
                     Menor precio
                 </NavLink>
-                <NavLink exact to='/orden/mayorPrecio'>
+                <NavLink activeClassName={'categorieSelected'} exact to='/orden/mayorPrecio'>
                     Mayor precio
                 </NavLink>
-            </div> */}
-        </> 
+            </div>
+        </div> 
     ) 
 }
