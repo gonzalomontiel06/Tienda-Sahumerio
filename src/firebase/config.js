d7030ad01f75b6db91bdda1900c29ec0825a2919
Gonzalo Montiel <gonzalo.montiel06@outlook.com>
2022-08-24 20:13:06 -0300

    data

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,12 @@ import 'firebase/firestore'
 // CONFIGURACION DE FIREBASE
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
+    apiKey: 'AIzaSyDDb8BDmmln7-alUEbSeqj2YE87tbAXPqU',
+    authDomain: 'loco-del-sahumerio.firebaseapp.com',
+    projectId: 'loco-del-sahumerio',
+    storageBucket: 'loco-del-sahumerio.appspot.com',
+    messagingSenderId: '285110095213',
+    appId: '1:285110095213:web:9237b8a6849a462b3d1e64'
 };
 
 const app = firebase.initializeApp(firebaseConfig)
@@ -21,3 +21,13 @@ export const getFirestore = () => {
 export const getAuth = () => {
     return firebase.auth(app)
 }
+
+
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_API_KEY,
+//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_APP_ID
+// };
